9da67e780d2c32ea68e7734a8faa7280207e99d3
ManaliSP <manalip054@gmail.com>
2021-05-02 10:23:21 +0530

    useeffect used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-import { useState } from 'react';
+
 function App() {
-    const [name, setName] = useState('Manali');
-    const [age, setAge] = useState('21');
-    const clickMe = () => {
-        setName('Patil');
-        setAge('22');
-    };
     return (
         <div className="App">
             <Navbar />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -11,13 +11,20 @@ const Home = () => {
             id: '3'
         }
     ]);
+    const [name, setName] = useState('Manali');
     const handleClick = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
+    useEffect(() => {
+        console.log("use effect ran");
+        console.log(name);
+    },[name])
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />
+            <h3>{name}</h3>
+            <button onClick={() => setName('Vaibhav')}>Change name</button>
         </div>
     );
 };
